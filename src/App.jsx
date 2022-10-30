2bdb0529edea70fe161a4e95f0c0872c34473bf9
RISHAV\USER <rishavnandi06@gmail.com>
2022-10-30 20:38:27 +0530

    navbar leftbar and rightbar  + themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,77 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Navbar from "./components/navbar/Navbar";
+import Leftbar from "./components/leftbar/Leftbar";
+import Rightbar from "./components/rightbar/Rightbar";
+import "./style.scss";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  const currentUser = true;
+  const Layout = () => {
+    return (
+      <div className="theme-dark" style={{ fontStyle: "Poppins,sans-serif" }}>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <Leftbar />
+          <div style={{ flex: "6" }}>
+            <Outlet />
+          </div>
+          <Rightbar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      home
+      {/* <Stories/>
+      <Share/>
+      <Posts/> */}
+    </div>
+  );
+};
+
+export default Home;
